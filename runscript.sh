22456a85a487cf5637fcb304b9db0e5bd4e2a941
amr <amr.atef@barqsystems.com>
2021-06-22 14:23:59 +0200

    change ssh key

--- runscript.sh
+++ runscript.sh
@@ -1,4 +1,4 @@
-ssh -i /var/lib/jenkins/.ssh/mynew_key ec2-user@172.31.21.220 <<EOF
+ssh -i .ssh/id_rsa ec2-user@172.31.21.220 <<EOF
 cd /usr/share/simple-node-js-react-npm-app
 serve -s build
 exit
